d9a78cfe1af222780aecfdc5a62b56adf30f86a8
lookatit113-ship-it <lookatit113@gmail.com>
2025-09-04 20:26:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // app.js
 
-// Check for service worker support
 if ('serviceWorker' in navigator) {
-    // Register the service worker after the page has loaded
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
+        // Change '/sw.js' to 'sw.js' or './sw.js'
+        navigator.serviceWorker.register('sw.js').then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         }).catch(error => {
             console.log('Service Worker registration failed:', error);
